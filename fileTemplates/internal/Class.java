2744f79153799379e447672e38da2df9af65122b
Denley <936489270@qq.com>
2019-05-03 11:30:38 +0800

    init

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+  *@className : ${NAME}
+  *@description : TODO
+  *@author : Denley
+  *@date : ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}:${SECOND}
+  *@version : 1.0
+  **/
+public class ${NAME} {
+}
